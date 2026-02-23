459128d91a82971c8be970eee6e6cb716c56c8c5
p4rksk <yeeun0118s@naver.com>
2026-02-23 16:36:52 +0900

    함수

--- basic.py
+++ basic.py
@@ -1,6 +1,10 @@
-battery = 70
+agent_bag = ["웹 검색", "웹 개발", "자동화"]
 
-if battery < 100:
-    print("배터리 충전 중...")
-else:
-    print(f"배터리 충전 완료! 배터리 충전량: {battery}%")
+def work_agent(work_name):
+    if work_name in agent_bag:
+        print(f"{work_name} 작업 하겠습니다.")
+    else:
+        print(f"{work_name}이 리스트에 없어 작업을 할 수 없습니다.")
+
+work_agent("자동화")
+
